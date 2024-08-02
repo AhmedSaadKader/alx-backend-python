dd9cd0e4dfdaac9e4f22bb530deb97a3badd5ffb
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-08-03 00:27:03 +0300

    .\1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that contains concat function that takes 2 strings and returns them
+concatenated
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two strings and returns them concatenated
+    """
+    return str1 + str2
