d410b1e8b3965500a03c3e6738a19bbca4458e78
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-08-03 00:44:50 +0300

    .\5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that contains sum_list which takes a list input_list of floats
+as argument and returns their sum as a float
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """takes input_list as argument and returns the sum of its items
+    """
+    sum: float = 0
+    for i in input_list:
+        sum += i
+    return sum
